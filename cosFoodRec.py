d6c8b1427f3715cdf4914fa4e1b2cc4d42937026
SeaofChaos <46305438+SeaofChaos@users.noreply.github.com>
2022-10-20 23:21:56 -0400

    Cleaned up cosFoodRec.py and added in the last functionality I had planned.
    In cosFoodRec.py:
    I added in two simple CLA's, one required for the dataset file, and the other a flag for using the recipe names in the recommendation algorithm. This can make it recommend recipes that have a similar title instead of basing it solely on ingredients.
    I also cleaned up main and added in a few functions for readability.
    
    I believe I am finished with cosFoodRec.py. If there are any more features I would like to implement they would probably be minor. I will now focus on either creating a simple web application for this program or I will change it up and make it work with another recommendation algorithm.

--- cosFoodRec.py
+++ cosFoodRec.py
@@ -12,45 +12,34 @@
 #
 ########################################################
 
-import numpy as np
 import pandas as pd
 import difflib
-import cv2
 import os
 import ast
-import sys
-import time
 import random
+import argparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 pd.options.mode.chained_assignment = None
 
-################################
-#   To do:
-#
-#   1. Limit file size 
-#       to prevent an enormous cosine matrix. 
-#   2. Make input file dynamic and
-#       not set to the 15k file every time. 
-################################
-
-def main():
-    if len(sys.argv) != 2:
-        exit("Usage: python3 " + sys.argv[0] + " cleaned_dataset")
-
-    fileName = sys.argv[1]
-
-    if fileName not in os.listdir(os.getcwd() + '\\Cleaned_Datasets'):
-        exit("Invalid dataset file name.")
+def checkCLA():
+    #CLA argument definitions
+    parser = argparse.ArgumentParser("Copy recipe data set, clean it, and output in a new file.\n")
+    parser.add_argument('--datafile', '-f', type=str, required=True, help="File containing cleaned recipe data.")
+    parser.add_argument('--usetitle', '-t', action='store_true', help="Take into consideration the name of a recipe for recommendations.")
+    args = parser.parse_args()
 
     print("Loading data...", end='', flush=True)
     # import recipe data
-    recipeSheet = pd.read_csv(os.getcwd() + '\\Cleaned_Datasets\\' + fileName)
+    recipeSheet = pd.read_csv(os.getcwd() + '\\Cleaned_Datasets\\' + args.datafile)
 
     if len(recipeSheet) > 50000:
         exit("\nFile size limited to 50,000 rows to prevent high memory usage.")
+    
+    return recipeSheet, args
 
+def getRecipes(args, recipeSheet):
     recipes = recipeSheet['ingredients']
 
     print("done.\nGetting feature vector...", end='', flush=True)
@@ -62,86 +51,64 @@ def main():
     recipes = [' '.join(recipe) for recipe in recipes]
     
     #If titles are used in feature vector, the similarity will be more biased towards
-    #similarly named recipes. I have taken it out for now but it can be useful for
-    #getting similar recipes. Since I want to recommend different recipes, it seems
-    #more useful to base it on ingredients alone.
+    #similarly named recipes. I have added in a flag to use them as it can be useful
+    #for getting similar recipes.
 
-    # titles = recipeSheet['title']
-    # titles[:] = [' ' + s for s in recipeSheet['title']]
-
-    recipes = recipes #+ titles
+    if args.usetitle:
+        titles = recipeSheet['title']
+        titles[:] = [' ' + s for s in recipeSheet['title']]
+        recipes = recipes + titles
 
     #convert recipes into feature vectors
     vectorizer = TfidfVectorizer()
     featureVector = vectorizer.fit_transform(recipes)
 
-    # titles[:] = [s[1:] for s in recipeSheet['title']]
-
     print("done.\nGetting cosing similarity...", end='', flush=True)
 
-    #get cosine similarity between recipes
-    cosSim = cosine_similarity(featureVector)
-
-    print("done.\n", flush=True)
-
-    choice = 0
-    while choice not in [1, 2, 3]:
-        try:
-            choice = int(input("Enter number of choice:\n1. Enter recipe name.\n2. Random recipe.\n3. Quit\n"))
-        except:
-            print("Invalid choice, please enter a number 1-3")
-
-    if choice == 1:
-        #get valid recipe
-        while True:
-            recipeName = input("Enter recipe: ")
-            if recipeName not in recipeSheet['title'].tolist():
-                recipeName = difflib.get_close_matches(recipeName, recipeSheet['title'].tolist(), cutoff=0.5)
-                num = 0
-                try:
-                    num = int(input("No direct match found. Enter the corresponding"+
-                                " number (1-3) for the following recipes:\n"+
-                                "1. "+recipeName[0]+" \n2. "+
-                                (recipeName[1] if len(recipeName)>1 else "None")+"\n3. "+
-                                (recipeName[2] if len(recipeName)>2 else "None")+"\n"
-                                "\nor (4-5) for the following options:\n4. re-enter recipe name\n5. quit.\n"))
-                except:
-                    print("No close recipe names found. Try entering a different recipe name.")
-                
-                if (len(recipeName) == 1 and (num == 2 or num == 3)) or (len(recipeName) == 2 and num == 3):
-                    print("Please make a valid recipe selection")
-                    continue
-                
-                if (num == 1 or num == 2 or num == 3):
-                    recipeName = recipeName[num-1]
-                    break
-                elif (num == 4):
-                    continue
-                elif (num == 5):
-                    return
-                else:
-                    print("Invalid input. Please enter a new recipe.")
-            else:
-                break
-        #get the index of selected recipe
-        recipeIdx = recipeSheet[recipeSheet['title'] == recipeName]['index'].values[0]
-    elif choice == 2:
-        recipeName = random.choice(recipeSheet['title'])
-        #get the index of selected recipe
-        recipeIdx = recipeSheet[recipeSheet['title'] == recipeName]['index'].values[0]
-    else:
-        return        
-
-    print("\nFinding similar recipes to \"" + recipeName + '\"')
-    ingredients = ast.literal_eval((recipeSheet[recipeSheet['title'] == recipeName].values[0])[1])
-    print("Ingredients: ", ", ".join(ingredients))
-
-    #get list of this recipe's similarity scores
-    recSim = list(enumerate(cosSim[recipeIdx]))
-
-    #sort list so most similar recipes are at front
-    sortedSim = sorted(recSim, key=lambda x:x[1], reverse=True)
+    if args.usetitle:
+        print("\nUsing titles.")
+        titles[:] = [s[1:] for s in recipeSheet['title']]
     
+    return recipes, featureVector
+
+def getRecipeID(recipeSheet):
+    #get valid recipe
+    while True:
+        recipeName = input("Enter recipe: ")
+        if recipeName not in recipeSheet['title'].tolist():
+            recipeName = difflib.get_close_matches(recipeName, recipeSheet['title'].tolist(), cutoff=0.5)
+            num = 0
+            try:
+                num = int(input("No direct match found. Enter the corresponding"+
+                            " number (1-3) for the following recipes:\n"+
+                            "1. "+recipeName[0]+" \n2. "+
+                            (recipeName[1] if len(recipeName)>1 else "None")+"\n3. "+
+                            (recipeName[2] if len(recipeName)>2 else "None")+"\n"
+                            "\nor (4-5) for the following options:\n4. re-enter recipe name\n5. quit.\n"))
+            except:
+                print("No close recipe names found. Try entering a different recipe name.")
+            
+            if (len(recipeName) == 1 and (num == 2 or num == 3)) or (len(recipeName) == 2 and num == 3):
+                print("Please make a valid recipe selection")
+                continue
+            
+            if (num == 1 or num == 2 or num == 3):
+                recipeName = recipeName[num-1]
+                break
+            elif (num == 4):
+                continue
+            elif (num == 5):
+                return
+            else:
+                print("Invalid input. Please enter a new recipe.")
+        else:
+            break
+    #get the index of selected recipe
+    recipeIdx = recipeSheet[recipeSheet['title'] == recipeName]['index'].values[0]
+
+    return recipeIdx, recipeName
+
+def printRecipes(recipeSheet, sortedSim):
     print("\nRecommended recipes")
     print("----------------------------------------")
     i = 0
@@ -156,9 +123,9 @@ def main():
             ingredients = ast.literal_eval((recipeSheet[recipeSheet['index'] == sortedSim[i][0]].values[0])[1])
             print("Ingredients:", ", ".join(ingredients)) #ingredients
         except:
-            sortedSim.remove(recipeSheet[recipeSheet['index'] == sortedSim[i][0]])
-            #i+=1
-            continue
+            #error printing, error in cleaned data, or reached end of sortedSim
+            print("Error retrieving recipe information. Please enter a different recipe or select a different dataset.")
+            break
         #print out 5 recipes
         if j%5 == 0:
             print("----------------------------------------")
@@ -168,9 +135,50 @@ def main():
             print("\n----------------------------------------")
         j+=1
         print("")
-
     return
 
+def main():
+    #check CLA and open cleaned recipes file
+    recipeSheet, args = checkCLA()
+
+    #get recipes from file and a feature vector
+    recipes, featureVector = getRecipes(args, recipeSheet)
+
+    #get cosine similarity between recipes
+    cosSim = cosine_similarity(featureVector)
+
+    print("done.\n", flush=True)
+
+    #allow user to continue to enter different recipes into program
+    while True:
+        choice = 0
+        while choice not in [1, 2, 3]:
+            try:
+                choice = int(input("Enter number of choice:\n1. Enter recipe name.\n2. Random recipe.\n3. Quit\n"))
+            except:
+                print("Invalid choice, please enter a number 1-3")
+
+        if choice == 1:
+            recipeIdx, recipeName = getRecipeID(recipeSheet)
+        elif choice == 2:
+            recipeName = random.choice(recipeSheet['title'])
+            #get the index of selected recipe
+            recipeIdx = recipeSheet[recipeSheet['title'] == recipeName]['index'].values[0]
+        else:
+            return        
+
+        print("\nFinding similar recipes to \"" + recipeName + '\"')
+        ingredients = ast.literal_eval((recipeSheet[recipeSheet['title'] == recipeName].values[0])[1])
+        print("Ingredients: ", ", ".join(ingredients))
+
+        #get list of this recipe's similarity scores
+        recSim = list(enumerate(cosSim[recipeIdx]))
+
+        #sort list so most similar recipes are at front
+        sortedSim = sorted(recSim, key=lambda x:x[1], reverse=True)
+        
+        printRecipes(recipeSheet, sortedSim)
+    return
 
 if __name__ == "__main__":
     main()
